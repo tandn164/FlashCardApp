2fd7ad4aceb4b59b28bd37286d97c10a24913a8d
Nguyen Duc Tan <tan.nguyen@kiaisoft.com>
2020-10-15 17:08:08 +0700

    add login with mail

--- FlashCardApp/Controller/User/SignIn/SignInViewController.swift
+++ FlashCardApp/Controller/User/SignIn/SignInViewController.swift
@@ -22,16 +22,18 @@ class SignInViewController: BaseViewController {
     @IBOutlet weak var twitterImageView: UIImageView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
     
     weak var delegate: SignInDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         functionView.layer.cornerRadius = 20
+        signInButton.layer.cornerRadius = signInButton.frame.height/2
         navigationController?.setNavigationBarHidden(true, animated: false)
         setGesture()
+        setupTextField()
     }
-    
 }
 
 extension SignInViewController {
@@ -46,6 +48,7 @@ extension SignInViewController {
         let twitterTap = UITapGestureRecognizer(target: self, action: #selector(twitterLogin))
         twitterTap.delegate = self
         twitterImageView.addGestureRecognizer(twitterTap)
+        
     }
     
     @objc func dismissView() {
@@ -81,48 +84,15 @@ extension SignInViewController {
                         LoadingHud.hide()
                         return
                     } else {
-                        self.fetchFacebookUserInfomation(facebookResult)
+                        self.dismiss(animated: true, completion: {
+                            LoadingHud.hide()
+                            DataLocal.saveObject(1, forKey: AppKey.accountType)
+                            self.delegate?.didSignIn()
+                        })
                     }
                 }
             }
         }
-    }
-    
-    func fetchFacebookUserInfomation(_ result: LoginManagerLoginResult) {
-        let graphRequest : GraphRequest = GraphRequest(graphPath: "me",
-                                                       parameters: ["fields" : "id, name, gender"])
-        graphRequest.start(completionHandler: {[weak self] (connection,
-                                                            result,
-                                                            error) -> Void in
-            if let error = error {
-                UIAlertController.showError(message: error.localizedDescription)
-                LoadingHud.hide()
-                return
-            }
-            
-            guard let self = self,
-                  let result = result else {
-                LoadingHud.hide()
-                return
-            }
-            
-            if let user : NSString = (result as AnyObject).value(forKey: "name") as? NSString {
-                DataLocal.saveObject(user, forKey: AppKey.userName)
-            }
-            if let facebookId : NSString = (result as AnyObject).value(forKey: "id") as? NSString {
-                let profileURL = "https://graph.facebook.com/" + (facebookId as String) + "/picture?type=square"
-                DataLocal.saveObject(profileURL, forKey: AppKey.userImageURL)
-                print(profileURL)
-            }
-            if let gender : NSString = (result as AnyObject).value(forKey: "id") as? NSString {
-                DataLocal.saveObject(gender, forKey: AppKey.userGender)
-            }
-            self.dismiss(animated: true, completion: {
-                LoadingHud.hide()
-                self.delegate?.didSignIn()
-            })
-        })
-        
     }
     
     @objc func twitterLogin() {
@@ -136,7 +106,7 @@ extension SignInViewController {
                         UIAlertController.showError(message: error.localizedDescription)
                     }
                 } else {
-                    UIAlertController.showError(message: "User canceled")
+                    UIAlertController.showError(message: Localizable.userCanceled, tittle: "")
                 }
                 return
             }
@@ -146,16 +116,16 @@ extension SignInViewController {
             let credential = TwitterAuthProvider.credential(withToken: session.authToken,
                                                             secret: session.authTokenSecret)
             LoadingHud.show()
-        
+            
             Auth.auth().signIn(with: credential) { (result, error) in
                 if let error = error {
                     UIAlertController.showError(message: error.localizedDescription)
                     LoadingHud.hide()
                     return
                 } else {
-                    DataLocal.saveObject(session.userName, forKey: AppKey.userName)
                     self.dismiss(animated: true, completion: {
                         LoadingHud.hide()
+                        DataLocal.saveObject(2, forKey: AppKey.accountType)
                         self.delegate?.didSignIn()
                     })
                 }
@@ -176,6 +146,64 @@ extension SignInViewController {
     }
     
     @IBAction func signInPressed(_ sender: UIButton) {
-        
+        guard checkEdited() else {
+            return
+        }
+        guard let mail = emailTextField.text,
+              let password = passwordTextField.text else {
+            return
+        }
+        view.endEditing(true)
+        LoadingHud.show()
+        Auth.auth().signIn(withEmail: mail, password: password) {[weak self] (result, error) in
+            LoadingHud.hide()
+            if let error = error {
+                UIAlertController.showError(message: error.localizedDescription)
+                return
+            }
+            guard let self = self,
+                  let _ = result else {
+                return
+            }
+            self.dismiss(animated: true) {
+                DataLocal.saveObject(3, forKey: AppKey.accountType)
+                self.delegate?.didSignIn()
+            }
+        }
+    }
+    
+    @IBAction func forgotPasswordPressed(_ sender: UIButton) {
+        self.dismiss(animated: true) {
+            let viewController = ForgotPasswordViewController()
+            viewController.modalPresentationStyle = .overFullScreen
+            viewController.modalTransitionStyle = .crossDissolve
+            UIViewController.top()?.present(viewController, animated: true, completion: nil)
+        }
+    }
+}
+
+extension SignInViewController: UITextFieldDelegate {
+    
+    func setupTextField() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    func checkEdited() -> Bool {
+        var check = true
+        if emailTextField.text == "" {
+            emailTextField.placeholder = Localizable.warningEmail
+            check = false
+        }
+        if passwordTextField.text == "" {
+            passwordTextField.placeholder = Localizable.warningPassword
+            check = false
+        }
+        return check
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
     }
 }
--- FlashCardApp/Controller/User/SignUp/SignUpViewController.swift
+++ FlashCardApp/Controller/User/SignUp/SignUpViewController.swift
@@ -85,48 +85,15 @@ extension SignUpViewController {
                         LoadingHud.hide()
                         return
                     } else {
-                        self.fetchFacebookUserInfomation(facebookResult)
+                        self.dismiss(animated: true, completion: {
+                            LoadingHud.hide()
+                            DataLocal.saveObject(1, forKey: AppKey.accountType)
+                            self.delegate?.didSignUp()
+                        })
                     }
                 }
             }
         }
-    }
-    
-    func fetchFacebookUserInfomation(_ result: LoginManagerLoginResult) {
-        let graphRequest : GraphRequest = GraphRequest(graphPath: "me",
-                                                       parameters: ["fields" : "id, name, gender"])
-        graphRequest.start(completionHandler: {[weak self] (connection,
-                                                            result,
-                                                            error) -> Void in
-            if let error = error {
-                UIAlertController.showError(message: error.localizedDescription)
-                LoadingHud.hide()
-                return
-            }
-            
-            guard let self = self,
-                  let result = result else {
-                LoadingHud.hide()
-                return
-            }
-            
-            if let user : NSString = (result as AnyObject).value(forKey: "name") as? NSString {
-                DataLocal.saveObject(user, forKey: AppKey.userName)
-            }
-            if let facebookId : NSString = (result as AnyObject).value(forKey: "id") as? NSString {
-                let profileURL = "https://graph.facebook.com/" + (facebookId as String) + "/picture?type=square"
-                DataLocal.saveObject(profileURL, forKey: AppKey.userImageURL)
-                print(profileURL)
-            }
-            if let gender : NSString = (result as AnyObject).value(forKey: "id") as? NSString {
-                DataLocal.saveObject(gender, forKey: AppKey.userGender)
-            }
-            self.dismiss(animated: true, completion: {
-                LoadingHud.hide()
-                self.delegate?.didSignUp()
-            })
-        })
-        
     }
     
     @objc func twitterLogin() {
@@ -140,7 +107,7 @@ extension SignUpViewController {
                         UIAlertController.showError(message: error.localizedDescription)
                     }
                 } else {
-                    UIAlertController.showError(message: "User canceled")
+                    UIAlertController.showError(message: Localizable.userCanceled, tittle: "")
                 }
                 return
             }
@@ -157,9 +124,9 @@ extension SignUpViewController {
                     LoadingHud.hide()
                     return
                 } else {
-                    DataLocal.saveObject(session.userName, forKey: AppKey.userName)
                     self.dismiss(animated: true, completion: {
                         LoadingHud.hide()
+                        DataLocal.saveObject(2, forKey: AppKey.accountType)
                         self.delegate?.didSignUp()
                     })
                 }
@@ -175,6 +142,96 @@ extension SignUpViewController {
     }
     
     @IBAction func signUpPressed(_ sender: UIButton) {
-        
+        if checkEdited() {
+            view.endEditing(true)
+            register(auth: Auth.auth())
+        }
+    }
+    
+    func register(auth: Auth) {
+        guard let mail = emailTexField.text,
+              let password = passWordTextField.text else {
+            return
+        }
+        LoadingHud.show()
+        auth.createUser(withEmail: mail,
+                        password: password) {[weak self] (result, error) in
+            if let error = error {
+                UIAlertController.showError(message: error.localizedDescription)
+                LoadingHud.hide()
+            } else {
+                self?.sendMail()
+            }
+        }
+    }
+    
+    func sendMail() {
+        guard let user = Auth.auth().currentUser else {
+            LoadingHud.hide()
+            return
+        }
+        user.reload { (error) in
+            if let error = error {
+                UIAlertController.showError(message: error.localizedDescription)
+                LoadingHud.hide()
+            }
+            switch user.isEmailVerified {
+            case true:
+                UIAlertController.showError(message: Localizable.emailExisted, tittle: "")
+                try! Auth.auth().signOut()
+                LoadingHud.hide()
+                return
+            case false:
+                user.sendEmailVerification { (error) in
+                    if let error = error {
+                        UIAlertController.showError(message: error.localizedDescription)
+                        try! Auth.auth().signOut()
+                        LoadingHud.hide()
+                    } else {
+                        self.dismiss(animated: true, completion: {
+                            LoadingHud.hide()
+                            DataLocal.saveObject(3, forKey: AppKey.accountType)
+                            UIAlertController.showError(message: Localizable.checkVerifyMail, tittle: "")
+                            self.delegate?.didSignUp()
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension SignUpViewController: UITextFieldDelegate {
+    
+    func setupTextField() {
+        emailTexField.delegate = self
+        passWordTextField.delegate = self
+        confirmPassWordTextField.delegate = self
+    }
+    
+    func checkEdited() -> Bool {
+        var check = true
+        if emailTexField.text == "" {
+            emailTexField.placeholder = Localizable.warningEmail
+            check = false
+        }
+        if passWordTextField.text == "" {
+            passWordTextField.placeholder = Localizable.warningPassword
+            check = false
+        } else {
+            if confirmPassWordTextField.text != passWordTextField.text {
+                confirmPassWordTextField.text = ""
+                confirmPassWordTextField.attributedPlaceholder
+                    = NSAttributedString(string: Localizable.wrongConfirmPassword,
+                                         attributes: [NSAttributedString.Key.foregroundColor : UIColor.red])
+                check = false
+            }
+        }
+        return check
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
     }
 }
